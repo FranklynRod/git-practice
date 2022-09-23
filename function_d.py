a2e0fbb7e755536a48c762cc728ebcace39b0370
Franklyn Rodriguez <franklynrodriguez.swe@gmail.com>
2022-09-23 17:08:58 -0400

    Changed function_d

--- function_d.py
+++ function_d.py
@@ -1,9 +1,6 @@
 def max_value(numbers):
-    """ This function returns the largest number
-        in the list.
-    """
-    pass
-
+   
+    print("")
 
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
